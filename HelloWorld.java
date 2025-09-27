ae89c09cb777841943efa449a9c360deb7f3b5f8
Vinh Tran <tbvinh@gmail.com>
2025-09-27 17:23:31 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Vinh 1 Tran - Hello World!");
+        System.out.println("Vinh Tran - Hello World!");
     }
 }
